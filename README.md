eeec0b093519efd8d6e1fa96766aa8a7cfc214b7
abhinav1912 <abhinavmathur1912@gmail.com>
2020-05-17 00:22:50 +0530

    Added server.py

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Socket-Program
+Create a basic socket model (with clients and a server) on the local network.
---
+++ server.py
@@ -0,0 +1,41 @@
+import socket
+import threading
+
+'''
+1. Define PORT for communication
+2. Define SERVER with IP Address of host machine
+3. Define the default address structure
+4. Define the address family (IPv4 for AF_INET here) and connection protocol (TCP for Sock_Stream)
+'''
+
+HEADER = 64
+PORT = 5050 
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+        print (f"[{addr}] {msg}")
+    conn.close()
+
+def start():
+    print ("[STARTING] Server is starting...")
+    server.listen()
+    print (f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+
+start()
