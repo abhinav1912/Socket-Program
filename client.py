ebf050c35d2121402d96e59962b211cb2373eb85
abhinav1912 <abhinavmathur1912@gmail.com>
2020-05-17 00:58:38 +0530

    Added client.py and updated server.py

---
+++ client.py
@@ -0,0 +1,23 @@
+import socket
+
+HEADER = 64
+PORT = 5050 
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+message = input()
+send(message)
+send(DISCONNECT_MESSAGE)
--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import threading
 2. Define SERVER with IP Address of host machine
 3. Define the default address structure
 4. Define the address family (IPv4 for AF_INET here) and connection protocol (TCP for Sock_Stream)
+5. Define handle_client function that will be used for threads.
 '''
 
 HEADER = 64
@@ -18,15 +19,16 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
+    print(f"[NEW CONNECTION] {addr} connected.\n")
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        msg_length = int(msg_length)
-        msg = conn.recv(msg_length).decode(FORMAT)
-        if msg == DISCONNECT_MESSAGE:
-            connected = False
-        print (f"[{addr}] {msg}")
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            print (f"[{addr}] {msg}")
     conn.close()
 
 def start():
@@ -37,5 +39,6 @@ def start():
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
+        print (f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
 start()
